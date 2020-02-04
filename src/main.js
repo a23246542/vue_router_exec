0a49f9197fe337abaa6c5d9978fab7d7f087f7bb
a23246542 <a23246542@gmail.com>
2020-02-04 16:27:33 +0800

    204

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+//官方預設載入
 import Vue from 'vue'
 import App from './App.vue'
+//router跟vuex
 import router from './router'
 import store from './store'
 
+
+
 Vue.config.productionTip = false
 
+//手牽手
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App) //渲染他的App function(handler)
+}).$mount('#app') //好了之後mount到#app
+//把什麼東西render出來呢 App.vue
